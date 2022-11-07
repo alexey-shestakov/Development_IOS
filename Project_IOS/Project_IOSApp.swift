926cf59c93940c880198d221cf99507a0084fb75
Алексей Шестаков <shestakad@gmail.com>
2022-11-07 09:36:30 +0300

    Initial Commit

---
+++ Project_IOS/Project_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_IOSApp.swift
+//  Project_IOS
+//
+//  Created by Alexey Shestakov on 07.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct Project_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
